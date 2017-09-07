365ed2e7e059f7db599b0bab4b3bc42755b20ccf
dan <daniel.munozdev@gmail.com>
2017-09-07 12:46:18 -0700

    Add 'click' events for index.html

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const outer = document.querySelector('.outer');
+console.log(outer);
+const inner = document.querySelector('.inner');
+const reset = document.querySelector('button');
+
+
+function onOuterClick() {
+  outer.classList.add('selected');
+  console.log('Outer clicked!');
+}
+
+function onInnerClick(event) {
+  inner.classList.add('selected');
+  console.log('Inner clicked!');
+  event.stopPropagation(); /*stops event bubbling */
+}
+
+function onResetClick() {
+  inner.classList.remove('selected');
+  outer.classList.remove('selected');
+}
+outer.addEventListener('click', onOuterClick);
+inner.addEventListener('click', onInnerClick);
+reset.addEventListener('click', onResetClick);
